604fa1ba5032703703cef8525de0a20096c9cd25
vivek kumar bondla <vivek.bondla@tcs.com>
2021-05-23 20:11:55 +0530

    first commoit

---
+++ src/server.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb'
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const withDB= async (operations,res)=>{
+    try{
+        const client = await  MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser:true});
+        const db = client.db('my-blog');
+        await operations(db);
+        client.close();
+
+    }catch(error){
+        res.status(500).json({message:'error connecting to db',error});
+    }
+}
+
+{/* get request from db*/}
+app.get('/api/articles/:name/', async (req,res) => {
+     withDB(async (db) =>{
+            const articleName=req.params.name;
+            const articleInfo = await db.collection('articles').findOne({name : articleName});
+            res.status(200).json(articleInfo);
+        },res);
+    
+});
+
+{/* post request to update the upvotes and return the response to client*/}
+app.post('/api/articles/:name/upvote', async (req,res)=>{
+    
+    withDB(async (db)=>{
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({name : articleName});
+        await db.collection('articles').updateOne({name : articleName}, {
+            '$set' : {
+                upvotes : articleInfo.upvotes+1 
+            },
+
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({name : articleName});
+        res.status(200).json(updatedArticleInfo);          
+     },res);              
+});
+
+{/* post request to add comments and return the response to client*/}
+app.post('/api/articles/:name/add-comment', async (req,res)=>{
+    withDB( async (db)=>{
+        const {username,text} = req.body;
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({name : articleName});
+        await db.collection('articles').updateOne({name : articleName}, {
+            '$set' : {
+                comments : articleInfo.comments.concat({username,text})
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({name:articleName});
+        res.status(200).json(updatedArticleInfo);
+    },res);
+});
+{/*app.get('/hello/:name',(req,res)=>res.send(`Hello ${req.params.name}!`));
+app.get('/hello',(req,res)=>res.send("Hello"));
+app.post('/hello',(req,res)=>res.send(`Hello${req.body.name}!`));*/}
+app.listen(8000,()=>console.log('listening on 8000'));
